5dd5d5a636c63592733bc6f86fefcca979baaedb
oloc <oloc@aurore>
2014-12-31 15:02:16 +0100

    Add manifests

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,17 @@ group { 'opt':
 	ensure => 'present',
 }
 
-user { 'oloc':
+user { 'admin account':
+	name => 'oloc',
 	ensure => 'present',
+	groups => [sudo, adm, opt],
+	home => '/home/oloc',
+	uid => 1000,
+}
+
+file { 'admin home':
+	path => '/home/oloc',
+	ensure => 'directory' ,
+	mode => 640,
+	owner => 'oloc',
 }
