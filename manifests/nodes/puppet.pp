3bb6194418d6e33659edf55eac1ac9d7ab89775a
oloc <oloc@aurore>
2015-01-21 17:19:28 +0100

    Modules-1575 is declared resolved - but it still does not work - Details: https://tickets.puppetlabs.com/browse/MODULES-1575

--- manifests/nodes/puppet.pp
+++ manifests/nodes/puppet.pp
@@ -1,27 +1,29 @@
 node puppet {
 	include g10b-node
-	include '::mysql::server'
 
 	$dshbrd_usr='dshbrd'
 	$dshbrd_grp='dshgrp'
 	$dshbrd_pwd='dshpwd'
 
-#	class { '::mysql::server':
-#		root_password => $mysql_password,
-#	}
 
-	class { 'dashboard':
-		dashboard_ensure   => 'present',
-		dashboard_user     => $dshbrd_usr,
-		dashboard_group    => $dshbrd_grp,
-		dashboard_password => $dshbrd_pwd,
-		dashboard_db       => 'dashboard_prod',
-		mysql_root_pw      => $mysql_password,
-		dashboard_charset  => 'utf8',
-        dashboard_site     => $fqdn,
-        dashboard_port     => '8080',
-        passenger          => true,
+	class { '::mysql::server':
+		root_password => $mysql_password,
 	}
+
+# Modules-1575 is declared resolved - Details: https://tickets.puppetlabs.com/browse/MODULES-1575
+# but it's still do not work
+#	class { 'dashboard':
+#		dashboard_ensure   => 'present',
+#		dashboard_user     => $dshbrd_usr,
+#		dashboard_group    => $dshbrd_grp,
+#		dashboard_password => $dshbrd_pwd,
+#		dashboard_db       => 'dashboard_prod',
+#		mysql_root_pw      => $mysql_password,
+#		dashboard_charset  => 'utf8',
+#       dashboard_site     => $fqdn,
+#       dashboard_port     => '8080',
+#       passenger          => true,
+#	}
 
 	cron{ 'Puppet Apply':
 		command => 'puppet apply $confdir/manifests --modulepath=$confdir/modules',
