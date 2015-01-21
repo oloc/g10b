969fc5d73aff75fe1a89fd877ae287216f8b6726
oloc <oloc@aurore>
2015-01-21 10:03:01 +0100

    Modules-1575 is resolved - Details: https://tickets.puppetlabs.com/browse/MODULES-1575

--- manifests/nodes/puppet.pp
+++ manifests/nodes/puppet.pp
@@ -1,6 +1,22 @@
 node puppet {
 	include g10b-node
-	#include g10b-mysql::mysql
+
+	$dshbrd_usr='dshbrd'
+	$dshbrd_grp='dshgrp'
+	$dshbrd_pwd='dshpwd'
+
+	class { '::mysql::server':
+		root_password => $mysql_password,
+	}
+
+	class { 'dashboard':
+		dashboard_ensure   => 'present',
+		dashboard_user     => $dshbrd_usr,
+		dashboard_group    => $dshbrd_grp,
+		dashboard_password => $dshbrd_pwd,
+		dashboard_db       => 'dashboard_prod',
+		mysql_root_pw      => $mysql_password,
+	}
 
 	cron{ 'Puppet Apply':
 		command => 'puppet apply $confdir/manifests --modulepath=$confdir/modules',
