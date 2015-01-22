0ffc7d35bdfe8f0e5532c45de1a5adf595b8f3b7
oloc <oloc@aurore>
2015-01-22 17:58:58 +0100

    Add needed extra packages for GitLab installation

--- manifests/nodes/gitlab.pp
+++ manifests/nodes/gitlab.pp
@@ -1,6 +1,6 @@
 node gitlab {
 	include g10b-node
-#	include g10b-mysql
+	#include g10b-mysql
 	#include nginx
 
 	$gitlab_dbname = 'gitlab'
@@ -12,16 +12,31 @@ node gitlab {
 	#	gitlab_dbuser => $gitlab_dbuser,
 	#	gitlab_dbpwd  => $gitlab_dbpwd,
 	#}
-
 	class { '::mysql::server':
     	root_password  =>  $mysql_password,
-    	databases => { $gitlab_dbname => { ensure => 'present' } }
+    	databases => { "$gitlab_dbname" => { ensure => 'present' } }
   	} ->
-	class {'gitlab':
+  	package { 'build-essential':
+		name => 'build-essential',
+		ensure => installed,
+		} ->
+	package { 'g++':
+		name => 'g++',
+		ensure => installed,
+	} ->
+	package { 'ruby1.9.3':
+		name => 'ruby1.9.3',
+		ensure => installed,
+	} ->
+	package { 'ruby-dev':
+		name => 'ruby-dev',
+		ensure => installed,
+	} ->
+	class { 'gitlab':
 		git_user          => 'gitlab',
 	    git_email         => 'notifs@foobar.fr',
 	    git_comment       => 'GitLab',
-	    gitlab_domain     => 'gitlab.foobar.fr',
+	    gitlab_domain     => 'gitlab.oloc',
 	    gitlab_dbtype     => 'mysql',
 	    gitlab_dbname     => $gitlab_dbname,
 	    gitlab_dbuser     => $gitlab_dbuser,
