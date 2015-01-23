c532ba6aa4cfca2f7bb47a88eecaf575d2c7b76c
oloc <oloc@aurore>
2015-01-23 10:39:01 +0100

    Fix: add the gitlab-ctl reconfigure

--- manifests/nodes/gitlab.pp
+++ manifests/nodes/gitlab.pp
@@ -2,9 +2,9 @@ node gitlab {
 	include g10b-node
 
 	class { 'gitlab' : 
-		puppet_manage_config   => false,
-		gitlab_branch          => '7.0.0',
-		external_url           => 'http://gitlab.example.com',
+		puppet_manage_config => true,
+		gitlab_branch        => '7.0.0',
+		external_url         => 'http://gitlab.oloc',
 	}
 
 }
