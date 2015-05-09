e0fbd0953dbcb16997cc0c82fe678b65731bb14e
oloc <oloc@aurore>
2015-05-09 11:30:22 +0200

    Definition of g10b::route

--- modules/g10b/manifests/init.pp
+++ modules/g10b/manifests/init.pp
@@ -5,7 +5,7 @@ class g10b {
 	include g10b::cron	
 
 	$dnsservers = hiera('dnsclient::nameservers')
-	$subnet = hiera('subnet')
+
 
 	class { 'git': }
 
@@ -14,15 +14,6 @@ class g10b {
 		domain => "$::domain",
 	}
 
-	network::route {'eth0':
-		ipaddress => [ "10.0.2.0", ],
-		netmask   => [ '255.255.255.0', ],
-		gateway   => [ "*", ],
-	}
-	network::route {'eth1':
-		ipaddress => [ 'default', "$subnet.0", ],
-		netmask   => [ '0.0.0.0', '255.255.255.0', ],
-		gateway   => [ "$subnet.50", "$subnet.50", ],
-	}
+	class { 'g10b::route': }
 
 }
