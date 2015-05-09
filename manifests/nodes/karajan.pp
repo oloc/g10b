6b135ca9412477e260bab65cebfaeef5448866f5
oloc <oloc@aurore>
2015-05-09 10:35:10 +0200

    Add mesos apt-key + Add rundeck stop before class

--- manifests/nodes/karajan.pp
+++ manifests/nodes/karajan.pp
@@ -6,9 +6,24 @@ node karajan {
 	$rd_grp='rundeck'
 
 	class { 'jenkins': }
+	
+	# Mesosphere
+	exec {'add_mesosphere-key':
+		command => "/usr/bin/apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF",
+		user    => 'root',
+	}->
+	exec {'add_mesosphere_repo':
+		command => '/bin/echo "deb http://repos.mesosphere.io/$(lsb_release -is | tr \'[:upper:]\' \'[:lower:]\') $(lsb_release -cs) main" | \
+				sudo /usr/bin/tee /etc/apt/sources.list.d/mesosphere.list',
+		creates => "/etc/apt/sources.list.d/mesosphere.list",
+		user    => 'root',
+	}->
+	exec {'/usr/bin/apt-get -y update':
+		user => 'root',
+	}->
 	class { 'mesos::master': }
 
-
+	# Rundeck
 	group { 'Rundeck Group':
 		name => $rd_grp,
 		ensure => 'present',
@@ -22,6 +37,9 @@ node karajan {
 		home    => "/home/$rd_usr",
 	}
 
+	exec { 'service rundeckd stop':
+		user => 'root',
+	}->
 	class { 'rundeck':
 		user  => $rd_usr,
 		group => $rd_grp,
