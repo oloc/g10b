81cef47b47ad322618870f093007188d83866faf
oloc <oloc@aurore>
2015-05-20 21:48:59 +0200

    Modify all include into class for puppet.pp

--- manifests/nodes/puppet.pp
+++ manifests/nodes/puppet.pp
@@ -1,11 +1,10 @@
-node puppet {
-	include g10b
-	include g10b_ssh
-	
+node puppet {	
 	$dshbrd_usr='dshbrd'
 	$dshbrd_grp='dshgrp'
 	$dshbrd_pwd='dshpwd'
 
+	class {'g10b':}
+	class {'g10b::ssh':}
 
 	class { '::mysql::server':
 		root_password => $mysql_password,
