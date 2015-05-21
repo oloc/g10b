94ee35ef021d71637bac15046b3bbf9a3f4caa2c
oloc <oloc@aurore>
2015-05-21 17:42:04 +0200

    Remove artifacts form Dashboard tries

--- manifests/nodes/puppet.pp
+++ manifests/nodes/puppet.pp
@@ -1,13 +1,4 @@
 node puppet {	
-	$dshbrd_usr='dshbrd'
-	$dshbrd_grp='dshgrp'
-	$dshbrd_pwd='dshpwd'
-
 	class {'g10b':}
 	class {'g10b::ssh':}
-
-	class { '::mysql::server':
-		root_password => $mysql_password,
-	}
-
 }
