cde88bb8201a7b52360e557129ee106d6ba3406b
oloc <oloc@aurore>
2015-05-27 16:06:03 +0200

    Export of proxy_pass in the hieradata

--- modules/g10b/manifests/webserver.pp
+++ modules/g10b/manifests/webserver.pp
@@ -1,19 +1,16 @@
 class g10b::webserver {
   $project = hiera('project')
-  $jk_port = hiera('jenkins::port')
-  $gl_port = hiera('gitlab::port')
-  $ms_port = hiera('mesos::master_port')
-  $pp_port = '8140'
-  $rd_port = hiera('rundeck::port')
+  $pass = hiera('pass')
 
   class  { 'apache': }
 
-  file { "/var/www/${project}/":
-    ensure => directory,
-    group  => 'root',
-    mode   => '0775',
-    owner  => 'root',
-  }
+# Duplicate declaration modules/apache/manifests/vhost.pp:261
+#  file { "/var/www/${project}/":
+#    ensure => directory,
+#    group  => 'root',
+#    mode   => '0775',
+#    owner  => 'root',
+#  }
 
   file { 'index':
     ensure => file,
@@ -24,12 +21,6 @@ class g10b::webserver {
 
   apache::vhost { "${project}.${::domain}":
     docroot    => "/var/www/${project}/",
-    proxy_pass => [
-      "{ 'path' => '/puppet',  'url' => http://puppet.${::domain}:${pp_port}/ }",
-      "{ 'path' => '/rundeck', 'url' => http://karajan.${::domain}:${rd_port}/ }",
-      "{ 'path' => '/gitlab',  'url' => http://repositories.${::domain}:${gl_port}/gitlab }",
-      "{ 'path' => '/jenkins', 'url' => http://karajan.${::domain}:${jk_port}/jenkins }",
-      "{ 'path' => '/mesos',   'url' => http://karajan.${::domain}:${ms_port}/ }",
-    ],
+    proxy_pass => $pass,
   }
 }
