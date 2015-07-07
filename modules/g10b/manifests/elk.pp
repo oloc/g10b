620c951838b723936be03abe5ae14eb0308f346f
oloc <oloc@aurore>
2015-07-07 13:57:13 +0200

    ELK improvement

--- modules/g10b/manifests/elk.pp
+++ modules/g10b/manifests/elk.pp
@@ -1,16 +1,25 @@
 class g10b::elk(
   $kibana_user        = $g10b::elk::kibana_user,
   $kibana_group       = $g10b::elk::kibana_group,
+  $elk_host           = $g10b::elk::host,
+  $kibana_port        = $g10b::elk::kibana_port,
   $elasticsearch_port = $g10b::elk::elasticsearch_port,
 ) {
 
-  class {'::elasticsearch':}
+  class {'elasticsearch':}
+  elasticsearch::instance { 'es-01': 
+    ensure  => present,
+    require => Class['elasticsearch'],
+  }
+  
   class {'::logstash':}
 
   class {'::kibana4':
     manage_user       => true,
     kibana4_user      => $kibana_user,
     kibana4_group     => $kibana_group,
+    host              => $elk_host,
+    port              => $kibana_port,
     elasticsearch_url => "http://${::fqdn}:${elasticsearch_port}",
   }
 }
