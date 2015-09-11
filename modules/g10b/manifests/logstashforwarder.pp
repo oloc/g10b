9f1c68f356a01d8c4de8ad1ab7926eb483776573
oloc <oloc@aurore>
2015-09-11 17:18:41 +0200

    Correction key is in the private dir.

--- modules/g10b/manifests/logstashforwarder.pp
+++ modules/g10b/manifests/logstashforwarder.pp
@@ -8,7 +8,7 @@ class g10b::logstashforwarder(
     path   => $file_crt,
     source => "puppet:///modules/${module_name}/logstash-forwarder.crt",
   }
-  $file_key = '/etc/pki/tls/certs/logstash-forwarder.key'
+  $file_key = '/etc/pki/tls/private/logstash-forwarder.key'
   file {$file_key:
     path   => $file_key,
     source => "puppet:///modules/${module_name}/logstash-forwarder.key",
