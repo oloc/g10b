29c7b2b5ff35d59f98ede279d62ad828405a0885
oloc <oloc@aurore>
2015-09-22 14:39:10 +0200

    Jenkins: add docker image argument.

--- app/g10b/modules/g10b/manifests/job.pp
+++ app/g10b/modules/g10b/manifests/job.pp
@@ -1,16 +1,17 @@
 define g10b::job(
-  $project     = $module_name,
-  $application = $module_name,
-  $jobname     = $title,
-  $description = $title,
-  $git_url     = undef,
-  $git_branch  = 'master',
-  $template    = undef,
+  $project      = $module_name,
+  $application  = $module_name,
+  $jobname      = $title,
+  $description  = $title,
+  $git_url      = undef,
+  $git_branch   = 'master',
+  $docker_image = undef,
+  $template     = undef,
 ){
   include ::jenkins
 
   jenkins::job {$jobname:
-    config      => template("${module_name}/${template}"), 
+    config      => template("${module_name}/${template}"),
   }
 
 }
