57a783f0d771a9bfed4aaa0c325f4977e544ec62
oloc <oloc@aurore>
2015-09-22 16:08:23 +0200

    Jenkins: Docker plugin configuration.

--- app/g10b/manifests/karajan.pp
+++ app/g10b/manifests/karajan.pp
@@ -1,11 +1,12 @@
 node 'karajan' {
   class {'g10b':}
   class {'g10b::ssh':}
-  class {'g10b::rundeck':}
+  #class {'g10b::rundeck':}
   class {'g10b::mesos_master':}
   class {'g10b::jenkins':}
 
   class {'::maven':}
+  class {'::docker':}
 
   if !defined(Class['::java']) {
     class {'::java':
--- app/g10b/modules/g10b/manifests/jenkins.pp
+++ app/g10b/modules/g10b/manifests/jenkins.pp
@@ -2,17 +2,23 @@ class g10b::jenkins{
 
   class {'::jenkins':
     config_hash => { 'PREFIX' => { 'value' => '/jenkins' }, 'JENKINS_ARGS' => { 'value' => '--webroot=/var/cache/$NAME/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT --prefix=$PREFIX' } }
-  }->
-  file {'/var/lib/jenkins/hudson.tasks.Maven.xml':
-    ensure => present,
-    source => "puppet:///modules/${module_name}/hudson.tasks.Maven.xml",
   }
 
   $defaults = {
     'version' => 'latest',
+    'require' => Class['jenkins']
   }
   $plugins = hiera('jenkins::plugins')
   create_resources(jenkins::plugin, $plugins, $defaults)
+  ->
+  file {'/var/lib/jenkins/hudson.tasks.Maven.xml':
+    ensure => present,
+    source => "puppet:///modules/${module_name}/hudson.tasks.Maven.xml",
+  }->
+  file {'/var/lib/jenkins/org.jenkinsci.plugins.docker.commons.tools.DockerTool.xml':
+    ensure => present,
+    source => "puppet:///modules/${module_name}/org.jenkinsci.plugins.docker.commons.tools.DockerTool.xml",
+  }
 
   $jobs = hiera('jenkins::jobs')
   create_resources("${module_name}::job", $jobs)
