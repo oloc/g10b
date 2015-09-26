736c765dc22a98c820e887dc291b046c58931347
oloc <oloc@aurore>
2015-09-26 18:27:25 +0200

    Middleware Dockerfile correction.

--- app/petclinic/dockerfiles/middleware/site.pp
+++ app/petclinic/dockerfiles/middleware/site.pp
@@ -1,20 +1,12 @@
 class { 'tomcat': } ->
 class { 'java': } ->
-
 tomcat::instance { 'tomcat8':
-  catalina_base => '/opt/apache-tomcat/tomcat8',
   source_url    => 'http://mirrors.ircam.fr/pub/apache/tomcat/tomcat-8/v8.0.26/bin/apache-tomcat-8.0.26.tar.gz'
 }->
 tomcat::service { 'tomcat8':
   service_ensure => running,
-  catalina_home  => '/opt/apache-tomcat/tomcat8',
-}
-
-file { '/opt/apache-tomcat/tomcat8/webapps':
-  ensure => directory,
 }
 tomcat::war { 'petclinic.war':
-  catalina_base => '/opt/apache-tomcat/tomcat8',
-  app_base      => 'webapps',
+  catalina_base => '/opt/apache-tomcat',
   war_source    => 'http://192.168.10.56:8080/jenkins/view/petclinic/job/petclinic-build/ws/target/petclinic.war',
 }
