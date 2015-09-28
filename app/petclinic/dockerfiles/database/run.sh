a4fdc8abe9a5514f1d05c76ec771d286300b4238
oloc <oloc@aurore>
2015-09-28 16:21:53 +0200

    Add a workaround for the puppet-mysql in a docker contener.

--- app/petclinic/dockerfiles/database/run.sh
+++ app/petclinic/dockerfiles/database/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Workaround:
+# https://ask.puppetlabs.com/question/10955/issues-with-ubuntu-docker-image-and-puppetlabs-mysql/
+mv /sbin/initctl /sbin/oldinitctl
+echo -e '#!/bin/bash\nif [ $1 == "--version" ]\nthen\n  echo "initctl (upstart 1.12.1)"\nfi\n/sbin/oldinitctl "$@"' > /sbin/initctl
+chmod 755 /sbin/initctl
+
 puppet apply /root/site.pp
 # The container will run as long as the script is running,
 # i.e. starting a long-living process:
