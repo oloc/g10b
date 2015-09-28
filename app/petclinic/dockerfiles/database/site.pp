748fd4cfee9e4f2563876eeec606ec2a3b14603f
oloc <oloc@aurore>
2015-09-28 15:28:06 +0200

    Add sql creation script.

--- app/petclinic/dockerfiles/database/site.pp
+++ app/petclinic/dockerfiles/database/site.pp
@@ -1,1 +1,10 @@
 class {'::mysql::server':}
+
+mysql::db { 'petclinic':
+  user     => 'myuser',
+  password => 'mypass',
+  host     => 'localhost',
+  grant    => ['SELECT', 'UPDATE'],
+  sql      => '/root/initDB.sql',
+  import_timeout => 900,
+}
