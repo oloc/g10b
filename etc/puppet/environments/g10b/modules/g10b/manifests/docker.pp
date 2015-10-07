094c535758380023ecfe16bd72a6686f24c15833
oloc <oloc@aurore>
2015-10-07 10:47:14 +0200

    Style correction: Tab instead space

--- etc/puppet/environments/g10b/modules/g10b/manifests/docker.pp
+++ etc/puppet/environments/g10b/modules/g10b/manifests/docker.pp
@@ -3,8 +3,8 @@ class g10b::docker(
   $port = g10b::docker::port,
 ){
   class {'::docker':
-	tcp_bind         => 'tcp://127.0.0.1:4243',
-	extra_parameters => ["--insecure-registry ${host}:${port}"],
+    tcp_bind         => 'tcp://127.0.0.1:4243',
+    extra_parameters => ["--insecure-registry ${host}:${port}"],
   }
 
 }
